d79f10dcd4bdfc05155a05e6507023a8f12b2513
SrvZ <srvz39@gmail.com>
2024-01-27 15:57:08 +0000

    chore: code can be loaded in text area now

--- app/new.py
+++ app/new.py
@@ -8,6 +8,8 @@ from textual.containers import Container, VerticalScroll
 from textual.reactive import var
 from textual.widgets import DirectoryTree, Footer, Header, TextArea
 
+import os
+
 class CodeBrowser(App):
     """Textual code browser app."""
 
@@ -30,7 +32,7 @@ class CodeBrowser(App):
         with Container():
             yield DirectoryTree(path, id="tree-view")
             with VerticalScroll(id="code-view"):
-                yield TextArea("", id="code")
+                yield TextArea("This is a test!", id="code" )
         yield Footer()
 
     def on_mount(self) -> None:
@@ -42,11 +44,16 @@ class CodeBrowser(App):
         """Called when the user click a file in the directory tree."""
         event.stop()
         code_view = self.query_one("#code", TextArea)
+        self.load_file_edit_area(event.path, "#code")
         try:
-            with open(event.path, "r") as file:
-                content = file.read()
-                code_view.value = content
-                self.sub_title = str(event.path)
+            # with open(event.path, "r") as file:
+            #     content = file.read()
+            #     print(content)
+            #     code_view = self.query_one("#code", TextArea)
+            #     code_view.load_text(content)
+            #     self.sub_title = str(event.path)
+           pass 
+           # self.load_file_edit_area(event.path, "#code"
         except Exception:
             code_view.value = f"Error reading file: {event.path}"
             self.sub_title = "ERROR"
@@ -54,6 +61,31 @@ class CodeBrowser(App):
     def action_toggle_files(self) -> None:
         """Called in response to key binding."""
         self.show_tree = not self.show_tree
+    
+    def load_file_edit_area(self, filepath, textarea_id) -> None:
+        fileext = os.path.splitext(filepath)
+        lang_options = {
+            "py": "python",
+            "java": "java",
+            "js": "javascript",
+            "md": "markdown",
+        }
+        try:
+            file_lang = lang_options[fileext[1].replace(".","")]
+        except KeyError:
+            file_lang = None
+        
+        code_buff = self.query_one(textarea_id, TextArea)
+        print("test")
+        with open(filepath, 'r') as file:
+            file_content = file.read()
+            code_buff.theme = "dracula"
+            code_buff.language = file_lang
+            code_buff.load_text(file_content)
+            print(file_lang)
+            print(code_buff)
+            
+
 
 
 if __name__ == "__main__":
